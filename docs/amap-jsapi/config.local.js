668baee4c0d5700c2de55c1b071d5519995e78bb
userbobao <102653056+userbobao@users.noreply.github.com>
2026-06-23 09:34:22 +0800

    Update config.local.js

--- docs/amap-jsapi/config.local.js
+++ docs/amap-jsapi/config.local.js
@@ -2,8 +2,8 @@ window.G318_AMAP_CONFIG = {
   // GitHub Pages publishes this file publicly.
   // Fill only a Web JSAPI key here after restricting it to your GitHub Pages domain.
   // Do not put an Amap Web Service key or other private credentials here.
-  AMAP_JSAPI_KEY: "",
-  AMAP_SECURITY_JS_CODE: "",
+  AMAP_JSAPI_KEY: "08f6be2f112c4b5e83350cc93381a780",
+  AMAP_SECURITY_JS_CODE: "3043a09ea9de89c624e6609eb07b6ff3",
   DATA_URL: "../data/roadbook-map.final.json"
 };
 
